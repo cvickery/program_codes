839f2c96b1afc832c859134018bc995ba085228e
Christopher Vickery <Christopher.Vickery@qc.cuny.edu>
2016-09-28 15:12:04 -0400

    Add progress information to shell script

--- program_codes.sh
+++ program_codes.sh
@@ -10,22 +10,30 @@
 # Christopher Vickery
 # January 2016
 
+start_time=`date +%s`
 ymd=`date +%Y-%m-%d`
 (( n = 0 ))
-[[ -e program_codes.out ]] && mv program_codes.out program_codes.${ymd}.out
-for program_code in \
-  `curl --data "SEARCHES=1&instid=334000" http://www.nysed.gov/coms/rp090/IRPS2A 2>/dev/null |\
+program_codes=`curl --data "SEARCHES=1&instid=334000" http://www.nysed.gov/coms/rp090/IRPS2A 2>/dev/null |\
    sed -n 's/.*PROGCD=\([0-9]*\).*/\1/p'`
+num_codes=`echo $program_codes|wc -w`
+echo "There are $num_codes program codes"
+for program_code in $program_codes
 do
-  echo "Retrieving info for program code $program_code"
+ (( n = n + 1 ))
+  echo "Retrieving info for program code $n/$num_codes: $program_code"
 #  curl http://www.nysed.gov/COMS/RP090/IRPSL3?PROGCD=$program_code >> program_codes.raw
  curl http://www.nysed.gov/COMS/RP090/IRPSL3?PROGCD=$program_code 2>/dev/null | \
  ack "$program_code |FOR AWARD|PROGRAM FIN|PROGRAM PRO|CERTIFICATE|QUEENS" | \
  sed 's/<H4><PRE>//' >> program_codes.out
- (( n = n + 1 ))
 done
 echo "Retrieved info for $n program codes"
 
 # Generate spreadsheet from the program_codes.out file
-[[ -e program_codes.xlsx ]] && mv program_codes.xlsx program_codes.${ymd}.xlsx
 ./program_codes.py
+
+# Datestamp today’s files
+ mv program_codes.out program_codes.${ymd}.out
+ mv program_codes.xlsx program_codes.${ymd}.xlsx
+ end_time=`date +%s`
+
+ echo "Completed in $(( $end_time - $start_time )) seconds."
