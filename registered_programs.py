cd887f0f426f42a1ecf6575c11ef5daabbc5bfdf
Christopher Vickery <Christopher.Vickery@qc.cuny.edu>
2019-04-25 09:23:57 -0400

    Update registered_programs.py
    Preparation for use as a web application module.
    Encapsulate lookup_programs as a function; add options for generating html and
    updating db (not implemented yet); command line parses args.

--- registered_programs.py
+++ registered_programs.py
@@ -97,24 +97,15 @@ from cuny_institutions import cuny_institutions
 __author__ = 'Christopher Vickery'
 __version__ = 'April 2019'
 
-parser = argparse.ArgumentParser()
-parser.add_argument('institution', nargs='?', default='qns')
-parser.add_argument('-d', '--debug', action='store_true', default=False)
-parser.add_argument('-v', '--verbose', action='store_true', default=False)
-args = parser.parse_args()
 
-institution = args.institution.lower().strip('10')
-institution_id, institution_name = cuny_institutions[institution]
-
-
-def detail_lines(all_lines):
+def detail_lines(all_lines, debug=False):
   """ Filter out unwanted lines from a details web page for a program code, and generate the others.
   """
   lines = all_lines.splitlines()
   for line in lines:
     if re.search(r'^\s+\d{5}\s+|FOR AWARD|PROGRAM|CERTIFICATE|M/A|M/I', line):
       next_line = line.replace('<H4><PRE>', '').strip()
-      if args.debug:
+      if debug:
         print(next_line)
       yield next_line
 
@@ -132,9 +123,9 @@ def fix_title(str):
              .replace('Cuny', 'CUNY'))
 
 
-def update_program(program, institution, award=None, title=None, hegis=None):
-  """ Update info for a program. It matters whether the institution matches the global
-      institution_name or not.
+def update_program(program, institution, institution_name, award=None, title=None, hegis=None):
+  """ Update info for a program.
+      It matters whether the institution matches the institution_name or not.
   """
   if institution == institution_name:
     program.institution = fix_title(institution)
@@ -148,161 +139,188 @@ def update_program(program, institution, award=None, title=None, hegis=None):
     program.other_institution = f'{fix_title(institution)} “{fix_title(title)}” {hegis}, ({award})'
 
 
-# Fetch the web page with all QC program codes and their unit codes
-if args.verbose:
-  print(f'Fetching list of registered programs for {institution_name} ...', file=sys.stderr)
-r = requests.post('http://www.nysed.gov/coms/rp090/IRPS2A', data={'SEARCHES': '1',
-                                                                  'instid': f'{institution_id}'})
-html_document = document_fromstring(r.content)
-# the program codes and unit codes are inside H4 elements, in the following sequence:
-#   PROGRAM CODE  : 36256 - ...
-#   PROGRAM TITLE : [title text] AWARD : [award text]
-#   INST.NAME/CITY ... (Includes hegis, but get that from details; ignored.)
-#   FORMAT ... (Not always present; ignored.)
-#   UNIT CODE     : OCUE|OP
-h4s = [h4.text_content() for h4 in html_document.cssselect('h4')]
-# [Waiting for assignment expressions in python 3.8 (PEP 572)]
-# h4s = [h4_line for h4 in html_document.cssselect('h4') if 'CODE' in h4_line := h4.text_content()]
+def lookup_programs(institution, verbose=False, debug=False):
+  """ Scrape info about academic programs registered with NYS from the Department of Education
+      website.
+  """
+  institution_id, institution_name = cuny_institutions[institution]
+  # Fetch the web page with program code, title, award, hegis, and unit code for all programs
+  # registered for the institution.
+  if verbose:
+    print(f'Fetching list of registered programs for {institution_name} ...', file=sys.stderr)
+  r = requests.post('http://www.nysed.gov/coms/rp090/IRPS2A', data={'SEARCHES': '1',
+                                                                    'instid': f'{institution_id}'})
+  html_document = document_fromstring(r.content)
+  # the program codes and unit codes are inside H4 elements, in the following sequence:
+  #   PROGRAM CODE  : 36256 - ...
+  #   PROGRAM TITLE : [title text] AWARD : [award text]
+  #   INST.NAME/CITY ... (Includes hegis, but get that from details; ignored.)
+  #   FORMAT ... (Not always present; ignored.)
+  #   UNIT CODE     : OCUE|OP
+  h4s = [h4.text_content() for h4 in html_document.cssselect('h4')]
+  # [Waiting for assignment expressions in python 3.8 (PEP 572)]
+  # h4s = [h4_line for h4 in html_document.cssselect('h4') if 'CODE' in h4_line := h4.text_content()]
 
-for h4 in h4s:
-  if 'PROGRAM CODE' in h4 and 'PROGRAM TITLE ORDER' not in h4:
-    matches = re.match(r'^\s*PROGRAM CODE\s*:\s*(\d+)', h4)
-    assert matches is not None, f'Unrecognized program code line: {h4}'
-    p = Program(matches.group(1))
+  for h4 in h4s:
+    if 'PROGRAM CODE' in h4 and 'PROGRAM TITLE ORDER' not in h4:
+      matches = re.match(r'^\s*PROGRAM CODE\s*:\s*(\d+)', h4)
+      assert matches is not None, f'Unrecognized program code line: {h4}'
+      p = Program(matches.group(1))
 
-  if 'UNIT CODE' in h4:
-    matches = re.match(r'\s*UNIT CODE\s*:\s*(.+)\s*', h4)
-    assert matches is not None, f'Unrecognized unit code line: {h4}'
-    p.unit_code = matches.group(1).strip()
-if args.verbose:
-  num_programs = len(Program.programs)
-  len_num = len(str(num_programs))
-  print(f'Found {num_programs} registered programs.', file=sys.stderr)
-  print('Fetching details...', file=sys.stderr)
+    if 'UNIT CODE' in h4:
+      matches = re.match(r'\s*UNIT CODE\s*:\s*(.+)\s*', h4)
+      assert matches is not None, f'Unrecognized unit code line: {h4}'
+      p.unit_code = matches.group(1).strip()
+  if verbose:
+    num_programs = len(Program.programs)
+    len_num = len(str(num_programs))
+    print(f'Found {num_programs} registered programs.', file=sys.stderr)
+    print('Fetching details...', file=sys.stderr)
 
-programs_counter = 0
-for p in Program.programs:
-  program = Program.programs[p]
-  programs_counter += 1
-  if args.verbose:
-    print(f'Program {programs_counter:{len_num}}/{num_programs}: code {p}\r',
-          end='', file=sys.stderr)
-
-  for_award = None
-  r = requests.get(f'http://www.nysed.gov/COMS/RP090/IRPSL3?PROGCD={program.program_code}')
-  for line in detail_lines(r.text):
-    # Use the first token on a line to determine the type of line, and dispatch to proper handler.
-    tokens = line.split()
-    token = tokens[0]
-
-    # There is a numeric string (Program Code #.) at the beginning.
-    if token.isdecimal():
-      # Extract program_code, title, hegis_code, award, institution.
-      matches = re.match(r'\s*\d+(.+)(\d{4}\.\d{2})\s+(\S+\s?\S*)\s+(.+)', line)
-      if matches is None:
-        sys.exit(f'\nUnable to parse program code line for program code {program_code}:\n{line}')
-      program_title = fix_title(matches.group(1))
-      program_hegis = matches.group(2)
-      program_award = matches.group(3).strip()
-      program_institution = matches.group(4)
-      if args.debug:
-        print(f'{program.program_code}: "{program_title}" {program_hegis}'
-              f' {program_award} "{program_institution}"')
-      update_program(program,
-                     title=program_title,
-                     hegis=program_hegis,
-                     institution=program_institution,
-                     award=program_award)
-
-    if token == 'M/A':
-      # line_type.multiple_awards
-      # Extract title, hegis, award, and institution
-      matches = re.match(r'\s*M/A(.+)(\d{4}\.\d{2})\s+(\S+\s?\S*)\s+(.+)', line)
-      if matches is None:
-        sys.exit(f'\nUnable to parse M/A line for program code {program_code}:\n{line}')
-      program_title = fix_title(matches.group(1))
-      program_hegis = matches.group(2)
-      program_award = matches.group(3).strip()
-      program_institution = matches.group(4)
-      if args.debug:
-        print(f'{program.program_code}: "{program_title}" {program_hegis}'
-              f' {program_award} "{program_institution}"')
-      update_program(program,
-                     title=program_title,
-                     hegis=program_hegis,
-                     institution=program_institution,
-                     award=program_award)
-
-    if token == 'M/I':
-      # Multi-institution lines add no new information about a program because multi-award lines
-      # already tell what the institution(s) are.
-      # But parsing code remains ... “just in case”
-      # Extract hegis, award, institution
-      #   But there is no hegis if the award is NOT-GRANTING
-      if 'NOT-GRANTING' in line:
-        program_hegis = None
-        program_award = None
-        program_institution = line.split('NOT-GRANTING ')[1].strip()
-      else:
-        matches = re.search(r'(\d{4}.\d{2})\s+(\S+\s?\S*)\s+(.*)', line)
-        if matches is None:
-          sys.exit(f'\nUnable to parse M/I line for program code {program.program_code}:\n{line}')
-        program_hegis = matches.group(1)
-        program_award = matches.group(2)
-        program_institution = matches.group(3).strip()
-      if args.debug:
-        print('M/I:', program_hegis, program_award, program_institution)
-
-    if token == 'FOR':
-      # Extract award. The following lines will not be of interest unless the award is in the list
-      # for this institution.
-      for_award = re.match(r'\s*FOR AWARD\s*--(.*)', line).group(1).strip()
-      if for_award not in program.award:
-        for_award = None
-
-    if token.startswith('CERTIFICATE') and for_award is not None:
-      # Extract certificate tuple {name, type, date} if there is one.
-      cert_info = re.sub(r'\s+', ' ', line.split(':')[1].strip())
-      if cert_info.startswith('NONE'):
-        cert_info = ''
-      program.certificate_license = cert_info
-
-    if token == 'PROGRAM' and tokens[1] == 'FINANCIAL' and for_award is not None:
-      # Extract three booleans.
-      matches = re.search(r'(YES|NO).+(YES|NO).+(YES|NO)', line)
-      if matches is None:
-        sys.exit('\nUnable to parse eligibility line for program code {}:\n{line}'
-                 .format(program.program_code))
-      program.tap = matches.group(1) == 'YES'
-      program.apts = matches.group(2) == 'YES'
-      program.vvta = matches.group(3) == 'YES'
-
-    if token == 'PROGRAM' and tokens[1] == 'PROFESSIONAL' and for_award is not None:
-      # Extract text, if any.
-      program.accreditation = line.split(':')[1].strip()
-
-    if token == 'PROGRAM' and tokens[1] == 'FIRST' and for_award is not None:
-      matches = re.search(r'DATE:\s+(\S+).+DATE:\s+(\S+)', line)
-      if matches is None:
-        sys.exit('\nUnable to parse registration dates for program code {}:\n{}'
-                 .format(program.program_code, line))
-      first_date = matches[1]
-      last_date = matches[2]
-      if (program.first_registration_date == 'Unknown'
-              or first_date.replace('PRE-', '19') < program.first_registration_date):
-        program.first_registration_date = first_date
-      if (program.last_registration_date == 'Unknown'
-              or last_date > program.last_registration_date):
-        program.last_registration_date = last_date
-
-if args.verbose:
-  print()
-
-# Generate spreadsheet
-#
-file_name = institution.upper() + '_' + date.today().isoformat() + '.csv'
-with open(file_name, 'w', newline='', encoding='utf-8') as csvfile:
-  writer = csv.writer(csvfile)
-  writer.writerow(Program.headings)
+  programs_counter = 0
   for p in Program.programs:
-    writer.writerow(Program.programs[p].values())
+    program = Program.programs[p]
+    programs_counter += 1
+    if verbose:
+      print(f'Program {programs_counter:{len_num}}/{num_programs}: code {p}\r',
+            end='', file=sys.stderr)
+
+    for_award = None
+    r = requests.get(f'http://www.nysed.gov/COMS/RP090/IRPSL3?PROGCD={program.program_code}')
+    for line in detail_lines(r.text):
+      # Use the first token on a line to determine the type of line, and dispatch to proper handler.
+      tokens = line.split()
+      token = tokens[0]
+
+      # There is a numeric string (Program Code #.) at the beginning.
+      if token.isdecimal():
+        # Extract program_code, title, hegis_code, award, institution.
+        matches = re.match(r'\s*\d+(.+)(\d{4}\.\d{2})\s+(\S+\s?\S*)\s+(.+)', line)
+        if matches is None:
+          sys.exit(f'\nUnable to parse program code line for program code {program_code}:\n{line}')
+        program_title = fix_title(matches.group(1))
+        program_hegis = matches.group(2)
+        program_award = matches.group(3).strip()
+        program_institution = matches.group(4)
+        if debug:
+          print(f'{program.program_code}: "{program_title}" {program_hegis}'
+                f' {program_award} "{program_institution}"')
+        update_program(program, program_institution, institution_name,
+                       title=program_title,
+                       hegis=program_hegis,
+                       award=program_award)
+
+      if token == 'M/A':
+        # line_type.multiple_awards
+        # Extract title, hegis, award, and institution
+        matches = re.match(r'\s*M/A(.+)(\d{4}\.\d{2})\s+(\S+\s?\S*)\s+(.+)', line)
+        if matches is None:
+          sys.exit(f'\nUnable to parse M/A line for program code {program_code}:\n{line}')
+        program_title = fix_title(matches.group(1))
+        program_hegis = matches.group(2)
+        program_award = matches.group(3).strip()
+        program_institution = matches.group(4)
+        if debug:
+          print(f'{program.program_code}: "{program_title}" {program_hegis}'
+                f' {program_award} "{program_institution}"')
+        update_program(program, program_institution, institution_name,
+                       title=program_title,
+                       hegis=program_hegis,
+                       award=program_award)
+
+      if token == 'M/I':
+        # Multi-institution lines add no new information about a program because multi-award lines
+        # already tell what the institution(s) are.
+        # But parsing code remains ... “just in case”
+        # Extract hegis, award, institution
+        #   But there is no hegis if the award is NOT-GRANTING
+        if 'NOT-GRANTING' in line:
+          program_hegis = None
+          program_award = None
+          program_institution = line.split('NOT-GRANTING ')[1].strip()
+        else:
+          matches = re.search(r'(\d{4}.\d{2})\s+(\S+\s?\S*)\s+(.*)', line)
+          if matches is None:
+            sys.exit(f'\nUnable to parse M/I line for program code {program.program_code}:\n{line}')
+          program_hegis = matches.group(1)
+          program_award = matches.group(2)
+          program_institution = matches.group(3).strip()
+        if debug:
+          print('M/I:', program_hegis, program_award, program_institution)
+
+      if token == 'FOR':
+        # Extract award. The following lines will not be of interest unless the award is in the list
+        # for this institution.
+        for_award = re.match(r'\s*FOR AWARD\s*--(.*)', line).group(1).strip()
+        if for_award not in program.award:
+          for_award = None
+
+      if token.startswith('CERTIFICATE') and for_award is not None:
+        # Extract certificate tuple {name, type, date} if there is one.
+        cert_info = re.sub(r'\s+', ' ', line.split(':')[1].strip())
+        if cert_info.startswith('NONE'):
+          cert_info = ''
+        program.certificate_license = cert_info
+
+      if token == 'PROGRAM' and tokens[1] == 'FINANCIAL' and for_award is not None:
+        # Extract three booleans.
+        matches = re.search(r'(YES|NO).+(YES|NO).+(YES|NO)', line)
+        if matches is None:
+          sys.exit('\nUnable to parse eligibility line for program code {}:\n{line}'
+                   .format(program.program_code))
+        program.tap = matches.group(1) == 'YES'
+        program.apts = matches.group(2) == 'YES'
+        program.vvta = matches.group(3) == 'YES'
+
+      if token == 'PROGRAM' and tokens[1] == 'PROFESSIONAL' and for_award is not None:
+        # Extract text, if any.
+        program.accreditation = line.split(':')[1].strip()
+
+      if token == 'PROGRAM' and tokens[1] == 'FIRST' and for_award is not None:
+        matches = re.search(r'DATE:\s+(\S+).+DATE:\s+(\S+)', line)
+        if matches is None:
+          sys.exit('\nUnable to parse registration dates for program code {}:\n{}'
+                   .format(program.program_code, line))
+        first_date = matches[1]
+        last_date = matches[2]
+        if (program.first_registration_date == 'Unknown'
+                or first_date.replace('PRE-', '19') < program.first_registration_date):
+          program.first_registration_date = first_date
+        if (program.last_registration_date == 'Unknown'
+                or last_date > program.last_registration_date):
+          program.last_registration_date = last_date
+  if verbose:
+    print()
+  return True
+
+
+if __name__ == '__main__':
+  parser = argparse.ArgumentParser()
+  parser.add_argument('institution')
+  parser.add_argument('-u', '--update_db', action='store_true', default=False,
+                      help='update info for this institution in the registered_programs database')
+  parser.add_argument('-w', '--html', action='store_true', default=False,
+                      help='generate a html table suitable for the web')
+  parser.add_argument('-c', '--csv', action='store_true', default=False,
+                      help='generate a CSV table')
+  parser.add_argument('-d', '--debug', action='store_true', default=False)
+  parser.add_argument('-v', '--verbose', action='store_true', default=False)
+  args = parser.parse_args()
+
+  institution = args.institution.lower().strip('10')
+  if lookup_programs(institution, debug=args.debug, verbose=args.verbose) is not None:
+    if args.csv:
+      # Generate spreadsheet
+      #
+      file_name = institution.upper() + '_' + date.today().isoformat() + '.csv'
+      with open(file_name, 'w', newline='', encoding='utf-8') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(Program.headings)
+        for p in Program.programs:
+          writer.writerow(Program.programs[p].values())
+    if args.html:
+      print('HTML option not implemented yet', file=sys.stderr)
+    if args.update_db:
+      print('Database not implemented yet', file=sys.stderr)
+  else:
+    sys.exit('lookup_programs failed')
