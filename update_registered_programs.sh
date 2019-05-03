236065fe09ffc3a7d2bd2a7be4637841b9c2d8a0
Christopher Vickery <Christopher.Vickery@qc.cuny.edu>
2019-05-03 10:22:46 -0400

    Update update_registered_programs
    Add .sh extension to name; generate .CSVs in csv_files; update the updates
    table.

--- update_registered_programs.sh
+++ update_registered_programs.sh
@@ -2,7 +2,7 @@
 for inst in bar bcc bkl bmc cty csi grd hos htr jjc kcc lag law leh mec \
 ncc nyt qcc qns sps yrk
 do
-  python3 registered_programs.py -cvu $inst >$inst.csv
+  python3 registered_programs.py -cvu $inst > csv_files/$inst.csv
 done
 psql cuny_courses -c "update updates set update_date = '`date -I`' \
                      where table_name = 'registered_programs'"
