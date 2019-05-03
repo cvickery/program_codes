4feb665684e4423c7b614c4ae4b2e32a7e0fa9b4
Christopher Vickery <Christopher.Vickery@qc.cuny.edu>
2019-05-03 11:13:43 -0400

    Update update_registered_programs.sh
    Move generated csv files into place correctly.

--- update_registered_programs.sh
+++ update_registered_programs.sh
@@ -2,7 +2,8 @@
 for inst in bar bcc bkl bmc cty csi grd hos htr jjc kcc lag law leh mec \
 ncc nyt qcc qns sps yrk
 do
-  python3 registered_programs.py -cvu $inst > csv_files/$inst.csv
+  python3 registered_programs.py -cvu $inst
+  mv `echo $inst|tr a-z A-Z`*csv csv_files
 done
 psql cuny_courses -c "update updates set update_date = '`date -I`' \
                      where table_name = 'registered_programs'"
