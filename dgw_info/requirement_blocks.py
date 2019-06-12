c3b13107ab3ae928eed8e2333db5584b06f29553
Christopher Vickery <Christopher.Vickery@qc.cuny.edu>
2019-06-12 10:23:31 -0400

    Update requirement_blocks.py
    Populate database tables updates and requirement_blocks.
    Need to edit the requirement_text field for coherent presentation.

--- dgw_info/requirement_blocks.py
+++ dgw_info/requirement_blocks.py
@@ -1,11 +1,13 @@
 """ Update requirement block information.
 """
 
+import argparse
 from pathlib import Path
 import csv
 import psycopg2
 from psycopg2.extras import NamedTupleCursor
 from datetime import datetime, timezone
+from collections import namedtuple
 
 # Not in DegreeWorks
 # GRD01 | The Graduate Center
@@ -36,6 +38,10 @@ from datetime import datetime, timezone
 # SP SPS01 | School of Professional Studies
 # YC YRK01 | York College
 
+parser = argparse.ArgumentParser()
+parser.add_argument('-d', '--debug', action='store_true', default=False)
+args = parser.parse_args()
+
 institution_mappings = {'bb': 'bar',
                         'bc': 'bkl',
                         'bm': 'bmc',
@@ -56,6 +62,22 @@ institution_mappings = {'bb': 'bar',
                         'si': 'csi',
                         'sp': 'sps',
                         'yc': 'yrk'}
+db_cols = ['institution',
+           'requirement_id',
+           'block_type',
+           'block_value',
+           'title',
+           'period_start',
+           'period_stop',
+           'major1',
+           'major2',
+           'concentration',
+           'minor',
+           'requirement_text']
+vals = '%s, ' * len(db_cols)
+vals = '(' + vals.strip(', ') + ')'
+
+DB_Record = namedtuple('DB_Record', db_cols)
 
 db = psycopg2.connect('dbname=cuny_programs')
 cursor = db.cursor(cursor_factory=NamedTupleCursor)
@@ -72,7 +94,29 @@ for query in queries.iterdir():
       if cursor.rowcount == 1:
         last_update = cursor.fetchone().last_update
       if cursor.rowcount == 0 or last_update < file_time:
-        print('AND NOW DO SOMETHING INTERESTING')
+        cols = None
+        db_records = []
+        with open(query) as csvfile:
+          reader = csv.reader(csvfile)
+          for row in reader:
+              if cols is None:
+                headings = [val.lower().replace(' ', '_').replace('/', '_') for val in row]
+                cols = {headings[i]: i for i in range(len(headings))}
+                if args.debug:
+                  print(cols.keys())
+                  for key in cols.keys():
+                    if key in db_cols:
+                      print(f'{key} ({cols[key]}) IS IN db_cols:')
+                    else:
+                      print(f'{key} ({cols[key]}) NOT IN db_cols')
+              else:
+                values = [row[cols[key]] for key in cols.keys() if key in db_cols]
+                assert len(values) + 1 == len(db_cols), f'{len(values)+1} is not {len(db_cols)}'
+                db_records.append(DB_Record._make([institution] + values))
+
+        cursor.execute(f"delete from requirement_blocks where institution = '{institution}'")
+        for db_record in db_records:
+          cursor.execute(f"insert into requirement_blocks values {vals}", (db_record))
         cursor.execute("""insert into updates values (%s, %s)
                               on conflict (institution)
                               do update set last_update = %s
