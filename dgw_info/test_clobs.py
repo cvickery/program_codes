228279d6bfee35bf90d033b694adcd47703760b2
Christopher Vickery <Christopher.Vickery@qc.cuny.edu>
2019-11-14 22:47:55 -0500

    Add test_clobs.py
    Test integrity of a csv file by checking if the requirement_text
    field has END. in it.

---
+++ dgw_info/test_clobs.py
@@ -0,0 +1,34 @@
+#! /usr/local/bin/python3
+""" Tell the sizes of CLOBS and whether or not they have END. lines
+    Used for integrity checks back in the days when the export form SQL Developer was truncating
+    them.
+"""
+
+
+import csv
+import sys
+from collections import namedtuple
+
+csv.field_size_limit(sys.maxsize)
+
+if len(sys.argv) != 2:
+  sys.exit('Usage: test_clobs file.csv')
+
+cols = None
+with open(sys.argv[1]) as csvfile:
+  reader = csv.reader(csvfile)
+  for line in reader:
+    if cols is None:
+      cols = [col.replace(' ', '_').lower() for col in line]
+      Row = namedtuple('Row', cols)
+    else:
+      if len(line) > 29:
+        row = Row._make(line)
+        if r'END.' not in row.requirement_text.upper():
+          no_end = 'NO END'
+        else:
+          no_end = 'END OK'
+        print(f'{row.institution} {row.period_stop:>10} {row.requirement_id} '
+              f'{row.block_type:6} {row.block_value:12} {len(row.requirement_text):6,} {no_end}')
+      else:
+        print(f'IGNORE |{line}|', file=sys.stderr)
